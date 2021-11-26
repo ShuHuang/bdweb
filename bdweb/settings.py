005bcca9f85fbe32f99fea0ad1a60579a4c304bf
Shu Huang <sh2009@cam.ac.uk>
2021-11-27 07:05:25 +0800

    user registration

--- bdweb/settings.py
+++ bdweb/settings.py
@@ -33,6 +33,8 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'about.apps.AboutConfig',
     'data.apps.DataConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -124,3 +126,6 @@ STATIC_URL = 'about/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Crispy form
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- bdweb/urls.py
+++ bdweb/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('about.urls')),
+    path('register/', user_views.register, name='register'),
     path('data/', include('data.urls')),
 ]
