f4bfbd5de451d7cc4366a5166162e71f9a3ecabd
Shu Huang <sh2009@cam.ac.uk>
2021-11-27 21:50:08 +0800

    debug

--- users/models.py
+++ users/models.py
@@ -10,10 +10,10 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self):
-        super().save()
+    def save(self, *args, **kwargs):
+        super(Profile, self).save(*args, **kwargs)
 
-        img = Image.open(self.image.path)
+        img = Image.open(self.image.path).convert('RGB')
 
         if img.height > 200 or img.width > 200:
             output_size = (200, 200)
