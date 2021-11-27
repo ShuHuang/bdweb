59937be758546082932b5c40bc2b0f00742b400a
Shu Huang <sh2009@cam.ac.uk>
2021-11-28 07:55:23 +0800

    reset password:

--- bdweb/settings.py
+++ bdweb/settings.py
@@ -140,3 +140,12 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'about-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'myoasis2020@gmail.com'
+EMAIL_HOST_PASSWORD = 'Tree950831'
+
+
