57f8cc84b352b70e20fc374c0e2860ca4373f070
Shu Huang <sh2009@cam.ac.uk>
2021-11-27 19:12:24 +0800

    user profile

--- bdweb/urls.py
+++ bdweb/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
 
 urlpatterns = [
@@ -27,3 +29,6 @@ urlpatterns = [
     path('data/', include('data.urls')),
     path('', include('about.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
