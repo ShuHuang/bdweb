2c09f73387b1b6e3cd5489e098d3fb34b8c5f2a8
Shu Huang <sh2009@cam.ac.uk>
2021-11-30 17:57:35 +0800

    remove register

--- data/urls.py
+++ data/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.propertydata, name='data-propertydata'),
-    # path('about/', views.about, name='about-about'),
 ]
--- data/views.py
+++ data/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+
 @login_required()
 def propertydata(request):
     return render(request, 'data/home.html')
