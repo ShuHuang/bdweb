1e6f6f4745cad114eea63b9559b8caa4d25fead0
Shu Huang <sh2009@cam.ac.uk>
2021-11-30 18:43:30 +0800

    contact and citing

--- about/urls.py
+++ about/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='about-home'),
     path('about/', views.about, name='about-about'),
+    path('citing/', views.citing, name='about-citing'),
+    path('contact/', views.contact, name='about-contact'),
 ]
