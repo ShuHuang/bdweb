d7152a27260549a363b800bcab64db2b744735c2
Shu Huang <sh2009@cam.ac.uk>
2021-12-04 01:19:23 +0800

    update

--- bdweb/settings.py
+++ bdweb/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'batteryqa.apps.BatteryqaConfig',
     'crispy_forms',
+    'macros',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
